768b529233a32c6aa221fe4a9e4f74f63ef58e74
Eric Dee <atallbirdmaker@protonmail.com>
2021-09-24 16:20:34 -0600

    Added descriptions for functions

--- Portfolio Modules/Sprint A3 - Calculator.js
+++ Portfolio Modules/Sprint A3 - Calculator.js
@@ -14,10 +14,15 @@ let lastOperation = "";
 let allowDecimal = true;
 let display = 1;
 
+//// This section listens for an event on a grid of buttons
+// It then runs it through a series of gates to determine which
+// mapping to follow ascertained by the exact key which has been pressed
 numbersElement.forEach((number) => {
   number.addEventListener("click", (e) => {
     let value = e.target.innerText;
     console.log(value);
+    // Take a value from the document constants above
+    // if a decimal is requested, ensure that it is only used one time
     switch (value) {
       case ".":
         if (allowDecimal === true) {
@@ -36,6 +41,7 @@ numbersElement.forEach((number) => {
         } else {
           break;
         }
+      // By default use the numbers clicked
       default:
         switch (display) {
           case 1:
@@ -52,6 +58,8 @@ numbersElement.forEach((number) => {
   });
 });
 
+//// This object holds certain instructions for each switch series
+// some people call this a "factory" setup
 let Operations = [
   { id: "EqualSwitch", status: false, symbol: "=" },
   { id: "PlusSwitch", status: false, symbol: "+" },
@@ -61,11 +69,21 @@ let Operations = [
   { id: "ModuloSwitch", status: false, symbol: "%" },
 ];
 
+//// This section will loop through each operation if one is clicked,
+// like the gate series above, only it only calls for action if a button
+// with an operation class has been pressed instead
 operationElement.forEach((operation) => {
   operation.addEventListener("click", (e) => {
     let key = e.target.innerText;
     console.log(key);
+
+    // This series of switches sets the operations object arrays
+    // to hold either true or false values depending on which item
+    // is pressed, and resets all others since only one operation can
+    // be applied per equality
     switch (key) {
+      // If the up down button is clicked, set the gate value
+      // that changes the location to display incoming numbers
       case "UpDwn":
         switch (display) {
           case 1:
@@ -76,11 +94,15 @@ operationElement.forEach((operation) => {
             break;
         }
         break;
+      // If equal is clicked, calculate based on the most recent
+      // operation requested
       case "=":
         var id = GetCurrentOperation();
         console.log(id);
         let calculatedValue = CalculateValue(id);
         break;
+      // These are all regular math operations which should be a given
+      // The operation objective booleans are set in these function calls
       case "+":
         OnOperationClick("PlusSwitch");
         break;
@@ -96,6 +118,7 @@ operationElement.forEach((operation) => {
       case "%":
         OnOperationClick("ModuloSwitch");
         break;
+      // If the screen is requested to be cleared clear it.
       case "CE":
         display1.innerText = "0";
         display2.innerText = "0";
@@ -118,6 +141,7 @@ operationElement.forEach((operation) => {
   });
 });
 
+// On first use, auto change the destination displayer
 let SwitchOnce = true;
 
 let OnOperationClick = (caller) => {
@@ -133,6 +157,8 @@ let OnOperationClick = (caller) => {
     SwitchOnce = false;
   }
 
+  // For each operation in the object array, check if its id matches what was passed in
+  // and if it does, set the gate status to allowed, and allow others to false
   Operations.forEach((operation) => {
     if (operation.id === caller) {
       operation.status = true;
@@ -144,7 +170,8 @@ let OnOperationClick = (caller) => {
   console.log(Operations);
 };
 
-function GetCurrentOperation() {
+// Find the operation that is presently true
+let GetCurrentOperation = () => {
   var flag;
   Operations.forEach((operation) => {
     if (operation.status) {
@@ -153,8 +180,9 @@ function GetCurrentOperation() {
     }
   });
   return flag;
-}
+};
 
+// Calculate a value using the operation sent in
 let CalculateValue = (operation) => {
   switch (operation) {
     case "PlusSwitch":
