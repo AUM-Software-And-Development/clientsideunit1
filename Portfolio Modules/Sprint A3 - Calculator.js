934fd8cbf631f9f40d04d4c781ea45cae183d4a0
Eric Dee <atallbirdmaker@protonmail.com>
2021-09-24 19:18:17 -0600

    Calculator submission

--- Portfolio Modules/Sprint A3 - Calculator.js
+++ Portfolio Modules/Sprint A3 - Calculator.js
@@ -6,6 +6,9 @@ const operationElement = document.querySelectorAll(".operation");
 const equalElement = document.querySelector(".equal");
 const clearElement = document.querySelector(".all-clear");
 const clearLastElement = document.querySelector(".clear-last-entity");
+const history = document.querySelector(".dynamiclist");
+const themebutton = document.querySelector(".theme-button");
+let themedcontainer = document.getElementById("dynamic-container");
 
 let display1Value = "";
 let display2Value = "";
@@ -21,6 +24,12 @@ numbersElement.forEach((number) => {
   number.addEventListener("click", (e) => {
     let value = e.target.innerText;
     console.log(value);
+
+    if (Reset) {
+      display = 1;
+      Reset = false;
+    }
+
     // Take a value from the document constants above
     // if a decimal is requested, ensure that it is only used one time
     switch (value) {
@@ -69,6 +78,12 @@ let Operations = [
   { id: "ModuloSwitch", status: false, symbol: "%" },
 ];
 
+//// This holds the calculated history index
+let element = 0;
+
+//// Reset display or start at the default
+let Reset = true;
+
 //// This section will loop through each operation if one is clicked,
 // like the gate series above, only it only calls for action if a button
 // with an operation class has been pressed instead
@@ -76,6 +91,11 @@ operationElement.forEach((operation) => {
   operation.addEventListener("click", (e) => {
     let key = e.target.innerText;
     console.log(key);
+
+    if (Reset) {
+      display = 1;
+      Reset = false;
+    }
 
     // This series of switches sets the operations object arrays
     // to hold either true or false values depending on which item
@@ -98,8 +118,17 @@ operationElement.forEach((operation) => {
       // operation requested
       case "=":
         var id = GetCurrentOperation();
-        console.log(id);
+        // Catch the calculated value
         let calculatedValue = CalculateValue(id);
+        // Apply the value or replace it if the index is full
+        if (element > 1) element = 0;
+        let zeroFill = document.createTextNode("");
+        let newText = document.createTextNode(
+          (element === 0 ? "x" : "y") + ": " + calculatedValue + " "
+        );
+        history.replaceChild(zeroFill, history.childNodes[0]);
+        history.replaceChild(newText, history.childNodes[element + 1]);
+        element++;
         break;
       // These are all regular math operations which should be a given
       // The operation objective booleans are set in these function calls
@@ -118,12 +147,10 @@ operationElement.forEach((operation) => {
       case "%":
         OnOperationClick("ModuloSwitch");
         break;
-      // If the screen is requested to be cleared clear it.
+      // If the screen is requested to be cleared reset all the switches
+      // and clear the documents history
       case "CE":
-        display1.innerText = "0";
-        display2.innerText = "0";
-        display1Value = "";
-        display2Value = "";
+        ResetSwitches();
         break;
       case "C":
         switch (display) {
@@ -206,4 +233,77 @@ let CalculateValue = (operation) => {
       display1.innerText = display1Value;
       break;
   }
+  return display1Value;
 };
+
+//// Sets all of the gate values to default states so nothing is confusing
+let ResetSwitches = () => {
+  display1.innerText = "0";
+  display2.innerText = "0";
+  display1Value = "";
+  display2Value = "";
+  Reset = true;
+  SwitchOnce = true;
+  let place1 = document.createTextNode("Data cleared");
+  let place2 = document.createTextNode("");
+  let place3 = document.createTextNode("");
+  history.replaceChild(place1, history.childNodes[0]);
+  history.replaceChild(place2, history.childNodes[1]);
+  history.replaceChild(place3, history.childNodes[2]);
+  element = 0;
+};
+
+//// On click method that gets fired every time the theme button is clicked
+let ChangeTheme = () => {
+  let className = themedcontainer.className;
+  // Switch case here to determine which class to assign to the container to apply the new theme
+  switch (className) {
+    case "default-container":
+      themedcontainer.className = "alternate-container";
+      break;
+    case "alternate-container":
+      themedcontainer.className = "default-container";
+      break;
+  }
+};
+
+//// Posting a message each time the theme gets changed to confirm
+let PostAMessage = () => {
+  switch (revert) {
+    case 0:
+      // If the id already exists then remove it. There should only be one
+      if (document.getElementById("volatilemessage")) {
+        document.getElementById("volatilemessage").remove();
+      }
+      // Assign and instantiate a new element with an id for the confirmation message
+      let casezero = document.createElement("h4");
+      casezero.setAttribute("id", "volatilemessage");
+      casezero.innerHTML = "The theme was changed.";
+      document.querySelector(".volatile-message").appendChild(casezero);
+      break;
+    case 1:
+      // Follow the same steps as above
+      if (document.getElementById("volatilemessage")) {
+        document.getElementById("volatilemessage").remove();
+      }
+      let caseone = document.createElement("h4");
+      caseone.setAttribute("id", "volatilemessage");
+      caseone.innerHTML = "The theme was changed back.";
+      document.querySelector(".volatile-message").appendChild(caseone);
+      break;
+  }
+};
+
+// Keep count of how many times the theme is being cycled so it wont get stuck on the same one
+let revert = 0;
+
+// Event listener to keep the changes moving in a forward direction
+themebutton.addEventListener("click", () => {
+  // If there are more requests than there are themes, then just start over
+  if (revert === 2) {
+    revert = 0;
+  }
+  ChangeTheme();
+  PostAMessage();
+  revert++;
+});
