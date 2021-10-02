2fba7d10f8042fb5db8ca6a5dbccd94fc4519bc6
Eric Dee <atallbirdmaker@protonmail.com>
2021-10-01 20:26:19 -0600

    Better functionality

--- Zoo Assignments/Sprint A4 - Zoo Assignment.js
+++ Zoo Assignments/Sprint A4 - Zoo Assignment.js
@@ -40,7 +40,7 @@ var VolatileZoo = null;
 document.getElementById("SourceAnimals").onclick = () => {
   // Zoo instantiate
   if (!First) {
-    EmptyCages(DefaultZoo);
+    EmptyTheCages(DefaultZoo);
   }
 
   DefaultZoo = new ZooInterface("Noah's Ark", 50, 1);
@@ -53,6 +53,7 @@ document.getElementById("SourceAnimals").onclick = () => {
       Weight: 3.2,
       Gender: "Female",
       IsPregnant: true,
+      Caged: true,
     },
 
     {
@@ -62,6 +63,7 @@ document.getElementById("SourceAnimals").onclick = () => {
       Weight: 3.2,
       Gender: "Male",
       IsPregnant: false,
+      Caged: true,
     },
 
     {
@@ -71,6 +73,7 @@ document.getElementById("SourceAnimals").onclick = () => {
       Weight: 852,
       Gender: "Female",
       IsPregnant: true,
+      Caged: true,
     },
 
     {
@@ -80,31 +83,11 @@ document.getElementById("SourceAnimals").onclick = () => {
       Weight: 3.2,
       Gender: "Female",
       IsPregnant: true,
+      Caged: true,
     }
   );
 
-  FillCages(DefaultZoo);
-
-  for (const property in DefaultZoo) {
-    console.log(
-      "Zoo property found: '" +
-        property +
-        "' ||| " +
-        "The value of the property is: '" +
-        DefaultZoo[property] +
-        "'"
-    );
-  }
-
-  for (let i = 0; i < DefaultZoo.capacity - 1; i++) {
-    DefaultZoo.numberOfGuests++;
-    if (DefaultZoo.numberOfGuests >= DefaultZoo.capacity) {
-      DefaultZoo.numberOfGuests = DefaultZoo.capacity;
-    }
-    console.log(
-      `The ${DefaultZoo.name}'s number of guests is: ${DefaultZoo.numberOfGuests}`
-    );
-  }
+  FillTheCages(DefaultZoo);
 
   RecursivelyBuildTheZoo(DefaultZoo);
 };
@@ -113,7 +96,7 @@ var LastAnimal = 0;
 
 function RecursivelyBuildTheZoo(Zoo) {
   // Define script elements to use
-  FillCages(Zoo);
+  FillTheCages(Zoo);
 
   // Define external elements
   const Form1 = new AnimalForm(
@@ -125,28 +108,21 @@ function RecursivelyBuildTheZoo(Zoo) {
     document.querySelector(".AnimalPregnancy")
   );
 
-  // Loop through animals and create a list item for each
-  Zoo.animals.forEach((animal) => {
-    let selectableItem = document.createElement("option");
-    selectableItem.value = animal.Name;
-    selectableItem.text = animal.Name;
-    Zoo.cages[0].SelectListBox.appendChild(selectableItem);
-  });
-
-  // Add the data to the listbox location
-  Zoo.cages[0].location.appendChild(Zoo.cages[0].SelectListBox);
+  SetupCageDisplays(Zoo);
 
   // Fill the animal form using the default animal
   if (!First) {
-    FillAnimalForm(Form1, Zoo.animals[LastAnimal].Name, Zoo);
+    FillTheAnimalForm(Form1, Zoo.animals[LastAnimal].Name, Zoo);
     Zoo.cages[0].SelectListBox[LastAnimal].selected = true;
   } else {
-    FillAnimalForm(Form1, "Perry", Zoo);
+    FillTheAnimalForm(Form1, "Perry", Zoo);
     // Code has ran. The default item gets selected
     Zoo.cages[0].SelectListBox[0].selected = true;
   }
 
   First = false;
+
+  OpenTheZoo(DefaultZoo);
 
   PostZooData(Zoo);
 
@@ -154,10 +130,12 @@ function RecursivelyBuildTheZoo(Zoo) {
   VolatileZoo = Zoo;
   delete Zoo;
 
+Runtime_EventListeners:
+
   // Gets the currently selected item each time the list box is clicked and fills the animal form
   VolatileZoo.cages[0].SelectListBox.onclick = () => {
     Zoo = VolatileZoo;
-    FillAnimalForm(
+    FillTheAnimalForm(
       Form1,
       Zoo.cages[0].SelectListBox.options[
         Zoo.cages[0].SelectListBox.selectedIndex
@@ -172,29 +150,53 @@ function RecursivelyBuildTheZoo(Zoo) {
 
   // User posts form data
   document.getElementById("AssignmentForm").addEventListener("submit", (e) => {
+    if (e.key === 'Enter')
+    {
+      Zoo = VolatileZoo;
+      delete VolatileZoo;
+      e.preventDefault();
+      ApplyUserFormRequests(Zoo);
+      RecursivelyBuildTheZoo(Zoo);
+    }
     Zoo = VolatileZoo;
     delete VolatileZoo;
     e.preventDefault();
-    let animalChanges = new FormData(document.getElementById("AssignmentForm"));
-    let objectReplacement = [];
-    for (let kvp of animalChanges.entries()) {
-      objectReplacement.push(kvp[1]);
-    }
-    let newAnimal = {
-      Name: objectReplacement[0],
-      Type: objectReplacement[1],
-      Age: objectReplacement[2],
-      Weight: objectReplacement[3],
-      Gender: objectReplacement[4],
-      IsPregnant: objectReplacement[5],
-    };
-    Zoo.animals[Zoo.cages[0].SelectListBox.selectedIndex] = newAnimal;
-    LastAnimal = Zoo.cages[0].SelectListBox.selectedIndex;
+    ApplyUserFormRequests(Zoo);
     RecursivelyBuildTheZoo(Zoo);
   });
+
+  document.querySelector(".add-animal").onclick = (e) => {
+    e.preventDefault();
+    Zoo = VolatileZoo;
+    delete VolatileZoo;
+    AddAnimalFromForm(Zoo);
+    RecursivelyBuildTheZoo(Zoo);
+  };
+
+  document.querySelector(".remove-animal").onclick = (e) => {
+    e.preventDefault();
+    Zoo = VolatileZoo;
+    delete VolatileZoo;
+    RemoveAnimalFromCage(Zoo);
+    RecursivelyBuildTheZoo(Zoo);
+  };
 }
 
-function FillCages(Zoo) {
+PlainFunctions:
+
+function OpenTheZoo(Zoo) {
+  for (let i = 0; i < Zoo.capacity - 1; i++) {
+    Zoo.numberOfGuests++;
+    if (Zoo.numberOfGuests >= Zoo.capacity) {
+      Zoo.numberOfGuests = Zoo.capacity;
+    }
+    console.log(
+      `The ${Zoo.name}'s number of guests is: ${Zoo.numberOfGuests}`
+    );
+  }
+}
+
+function FillTheCages(Zoo) {
   Zoo.cages.forEach((cage) => {
     cage.SelectListBox.remove();
   });
@@ -204,15 +206,31 @@ function FillCages(Zoo) {
   );
 }
 
-function EmptyCages(Zoo) {
+function EmptyTheCages(Zoo) {
   Zoo.cages.forEach((cage) => {
     cage.SelectListBox.remove();
   });
   Zoo.cages = [];
 }
 
+function SetupCageDisplays(Zoo) {
+    // Loop through animals and create a list item for each
+    Zoo.animals.forEach((animal) => {
+      if (animal.Caged)
+      {
+        let selectableItem = document.createElement("option");
+        selectableItem.value = animal.Name;
+        selectableItem.text = animal.Name;
+        Zoo.cages[0].SelectListBox.appendChild(selectableItem);
+      }
+    });
+  
+    // Add the data to the listbox location
+    Zoo.cages[0].location.appendChild(Zoo.cages[0].SelectListBox);
+}
+
 // Fills a form using the form itself as a base
-function FillAnimalForm(form, animalName, zoo) {
+function FillTheAnimalForm(form, animalName, zoo) {
   // Place each animal field into a form to edit
   let i = 0;
   for (let field in form) {
@@ -262,6 +280,7 @@ function GenerateTable(table, data) {
 function PostZooData(Zoo) {
   let ZooNameDivNode = document.querySelector(".Name");
   let ZooCapacityDivNode = document.querySelector(".Capacity");
+  let ZooAnimalNumberDivNode = document.querySelector(".NumberOfAnimals");
   let ZooGuestNumberDivNode = document.querySelector(".NumberOfGuests");
 
   let table = document.querySelector("table");
@@ -275,8 +294,86 @@ function PostZooData(Zoo) {
   ZooCapacityDivNode.innerHTML = "This zoo's capacity is: " + Zoo.capacity;
   ZooGuestNumberDivNode.innerHTML =
     "This zoo's current guest amount is: " + Zoo.numberOfGuests;
+  ZooAnimalNumberDivNode.innerHTML = "This zoo's current animal amount is: " + Zoo.animals.length;
 
   // Generate table first to get the table body
   GenerateTable(table, Zoo.animals);
   GenerateTableHead(table, data);
+}
+
+function ApplyUserFormRequests(Zoo) {
+  let animalChanges = new FormData(document.getElementById("AssignmentForm"));
+  let objectReplacement = [];
+  for (let kvp of animalChanges.entries()) {
+    objectReplacement.push(kvp[1]);
+  }
+  let newAnimal = {
+    Name: objectReplacement[0],
+    Type: objectReplacement[1],
+    Age: objectReplacement[2],
+    Weight: objectReplacement[3],
+    Gender: objectReplacement[4],
+    IsPregnant: objectReplacement[5],
+    Caged: true,
+  };
+  Zoo.animals.forEach((animal) => {
+    if (animal.Name === Zoo.cages[0].SelectListBox.options[Zoo.cages[0].SelectListBox.selectedIndex].value)
+    {
+      let cageStatus = false;
+      if (animal.Caged === true)
+      {
+       cageStatus = true;
+      }
+      newAnimal.Caged = cageStatus;
+      Object.assign(animal, newAnimal);
+      console.log(animal);
+    }
+  })
+  LastAnimal = 0;
+}
+
+function AddAnimalFromForm(Zoo) {
+  let animalInfo = new FormData(document.getElementById("AssignmentForm"));
+  let objectData = [];
+  let breaker = false;
+  for (let kvp of animalInfo.entries()) {
+    Zoo.animals.forEach((animal) => {
+      if (animal.Name === kvp[1])
+      {
+        animal.Caged = true;
+        window.alert("This animal has been added back to the cage. Change the name to add a new animal.");
+        breaker = true;
+        return;
+        // Javascript doesn't actually return to main from this point. It just returns to the for loop
+      }
+    })
+    if (breaker)
+    {
+      return;
+    }
+    objectData.push(kvp[1]);
+  }
+  let newAnimal = {
+    Name: objectData[0],
+    Type: objectData[1],
+    Age: objectData[2],
+    Weight: objectData[3],
+    Gender: objectData[4],
+    IsPregnant: objectData[5],
+    Caged: true,
+  };
+  Zoo.animals.push(newAnimal);
+  LastAnimal = 0;
+}
+
+function RemoveAnimalFromCage(Zoo) {
+  let nameForRemoval = Zoo.cages[0].SelectListBox.value;
+  Zoo.animals.forEach((animal) => {
+    if (animal.Name === nameForRemoval)
+    {
+      animal.Caged = false;
+    }
+  });
+
+  LastAnimal = 0;
 }
